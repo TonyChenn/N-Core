6ec9338cfadb725a571a459f9a5fa490d5faaab9
陈天赐(默然) <chentc@yoozoo.com>
2021-10-20 19:03:48 +0800

    add ETAsync

---
+++ Runtime/DataStruct/ETAsync/ETVoid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+
+[AsyncMethodBuilder(typeof(AsyncETVoidMethodBuilder))]
+public struct ETVoid : ICriticalNotifyCompletion
+{
+    [DebuggerHidden]
+    public void Coroutine()
+    {
+    }
+
+    [DebuggerHidden] public bool IsCompleted => true;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [DebuggerHidden]
+    public void OnCompleted(Action continuation)
+    {
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [DebuggerHidden]
+    public void UnsafeOnCompleted(Action continuation)
+    {
+    }
+}
