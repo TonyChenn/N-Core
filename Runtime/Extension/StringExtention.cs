c77abda579f90f7e4895280568ba139933204512
陈天赐(默然) <chentc@yoozoo.com>
2021-10-28 16:03:00 +0800

    1. add Md5Helper 2. update stringExtention

--- Runtime/Extension/StringExtention.cs
+++ Runtime/Extension/StringExtention.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using UnityEngine;
 
 namespace NextFramework.Core
@@ -37,6 +38,89 @@ namespace NextFramework.Core
                 return null;
             
             return $"Assets/{fullPath.Substring(Application.dataPath.Length)}";
+        }
+        #endregion
+
+        #region 文件夹
+
+        /// <summary>
+        /// 文件夹是否存在
+        /// </summary>
+        public static bool ExistFolder(this string folderPath)
+        {
+            if (string.IsNullOrEmpty(folderPath))
+                throw new Exception($"{folderPath} is null or empty");
+            
+            return Directory.Exists(folderPath);
+        }
+
+        /// <summary>
+        /// 创建文件夹（如果不存在）
+        /// </summary>
+        public static void CreateFolder(this string folderPath)
+        {
+            if (!folderPath.ExistFolder())
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+        }
+
+        /// <summary>
+        /// 删除文件夹（存在并且为空的话）
+        /// </summary>
+        public static void DeleteFolderIfEmpty(this string folderPath)
+        {
+            if (folderPath.ExistFolder())
+            {
+                if (Directory.GetDirectories(folderPath).Length == 0 && Directory.GetFiles(folderPath).Length == 0)
+                    Directory.Delete(folderPath);
+            }
+        }
+
+        /// <summary>
+        /// 删除文件夹（包括子文件/子文件夹也会删除）
+        /// </summary>
+        public static void DeleteFolderForce(this string folderPath)
+        {
+            if (folderPath.ExistFolder())
+            {
+                Directory.Delete(folderPath, true);
+            }
+        }
+        #endregion
+
+        #region 文件相关
+
+        /// <summary>
+        /// 判断文件是否存在
+        /// </summary>
+        public static bool ExistFile(this string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+                return false;
+            return File.Exists(filePath);
+        }
+
+        /// <summary>
+        /// 删除文件（如果存在）
+        /// </summary>
+        /// <param name="filePath"></param>
+        public static void DeleteFile(this string filePath)
+        {
+            if(filePath.ExistFile())
+                File.Delete(filePath);
+        }
+
+        /// <summary>
+        /// 创建文件的文件夹
+        /// </summary>
+        /// <param name="filePath"></param>
+        public static void CreateFileFolder(this string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath)) return;
+
+            string folderName = Path.GetDirectoryName(filePath);
+            folderName.CreateFolder();
         }
         #endregion
     }
